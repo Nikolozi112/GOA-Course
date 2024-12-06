e43cbe79d3de43a952844165cbd3f3ab042a4fab
Nikolozi112 <nikolozkap2010@gmail.com>
2024-12-06 23:06:36 +0400

    second commit

---
+++ Group 49/Level 24/Classwork/Classwork.py
@@ -0,0 +1,22 @@
+#1)
+my_list = [10, 20, 30, 40]
+print(len(my_list))  
+
+my_list.append(40)
+print(my_list)  
+
+my_list.insert(2, 12) 
+print(my_list) 
+
+removed_element = my_list.pop(3)  
+print(my_list)  
+print(removed_element)  
+
+my_list.remove(20)  
+print(my_list)  
+
+
+#4)
+my_list = [34, 73, 59, 64]
+my_list.insert(2, 12) 
+print(my_list)
---
+++ Group 49/Level 24/Homework/Homework.py
@@ -0,0 +1,28 @@
+#3)
+my_list = [10, 20, 30, 40, 50, 60]
+removed_element = my_list.pop(5)  
+print(my_list)
+
+#4)
+my_list = [10, 20, 30, 40]
+my_list.remove(20)  
+print(my_list)  
+
+#5)
+my_list = [10, 20, 30, 40]
+my_list.append(50)
+print(my_list)
+
+#6)
+my_list = [10, 20, 30, 40]
+my_list.insert(1, 15) 
+print(my_list) 
+
+#7)
+my_list = [10, 20, 30, 40]
+removed_element = my_list.pop(3)  
+print(my_list)
+
+#8)
+my_list = [10, 20, 30, 40]
+print(len(my_list))  
