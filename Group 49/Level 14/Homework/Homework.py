8cda2f5e80498437df91de165eac3ce88efb3327
Nikolozi112 <nikolozkap2010@gmail.com>
2024-12-12 00:24:33 +0400

    commit

--- Group 49/Level 14/Homework/Homework.py
+++ Group 49/Level 14/Homework/Homework.py
@@ -1,4 +1,4 @@
-#2)
+# 2) დაპრინტეთ Hello world 5'ჯერ (while loop'ით)
 
 x = 0
 
@@ -6,17 +6,17 @@ while x < 5:
     print("Hello world")
     x = x + 1
 
-#3)
+# 3) დაპრინტეთ Goodbye World 10'ჯერ (for loop'ით)
 
 for i in range(5):
     print("Goodbye world")
 
-#4)
+# 4) დაპრინტეთ რიცხვები 0'დან 100'ის ჩათვლით for loop'ის მეშვეობით 
 
 for i in range(101):
     print(i)
 
-#5)
+# 5) დაპრინტეთ რიცხვები 0'დან 100'ის ჩათვლით while loop'ის მეშვეობით 
 
 x = 0
 
@@ -24,12 +24,12 @@ while x < 101:
     print(x)
     x = x + 1
 
-#6)
+# 6) დაპრინტეთ რიცხვები 49'იდან 51'მდე for loop'ის მეშვეობით
 
 for i in range(49, 51):
     print(i)
 
-#7)
+# 7) დაპრინტეთ რიცხვები 16'იდან 57'მდე while loop'ის მეშვეობით
 
 x = 16
 
